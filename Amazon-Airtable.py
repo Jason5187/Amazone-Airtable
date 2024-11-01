e9d07ebc2d3b1fa59add37bbfa5c2b5992d024fd
Jason5187 <164372072+Jason5187@users.noreply.github.com>
2024-11-02 00:59:59 +0900

    Update Amazon-Airtable.py

--- Amazon-Airtable.py
+++ Amazon-Airtable.py
@@ -62,7 +62,7 @@ def amazon_crawling(amazon_url):
         else:
             return None
 
-    random_sec = random.uniform(2,4)
+    random_sec = random.uniform(4,6)
 
 
     driver = get_driver() #셀레니움 최신 버전에서는 자동으로 webdriver 설치 후 반영
@@ -70,7 +70,7 @@ def amazon_crawling(amazon_url):
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") # 셀레니움 디텍션 스크립트 비활성화
     
     # 이미지와 상세 정보 추출
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'li.a-spacing-small.item.imageThumbnail.a-declarative'))
     )
     sub_images = driver.find_elements(By.CSS_SELECTOR, 'li.a-spacing-small.item.imageThumbnail.a-declarative')
